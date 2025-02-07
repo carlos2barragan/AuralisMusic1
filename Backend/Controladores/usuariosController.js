a50e9760af6702d03ab84361d3242a1ec19dad08
nicolas herrera <nicoh3988@gmail.com>
2025-02-06 21:07:48 -0500

    endpoint usuarios terminados pero posible falla en postman

--- Backend/Controladores/usuariosController.js
+++ Backend/Controladores/usuariosController.js
@@ -1,6 +1,7 @@
 import Usuario from "../Modelos/usuariosModelos.js";
 import jwt from "jsonwebtoken"
 import bcrypt from "bcrypt"
+import mongoose from "mongoose";
 
 export const Registro = async (req, res) => {
   const { nombre, email, password } = req.body;
@@ -60,6 +61,11 @@ export const login = async (req, res) => {
         //  // Buscar al usuario en la base de datos
     const usuario = await Usuario.findOne({ email });
 
+    //Dejar que se puede leer el id de mongo
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID no válido" });
+    }
+
     if (!email || !password) {
         return res.status(400).json({ message: 'Email y contraseña son requeridos.' });
       }
@@ -109,6 +115,64 @@ export const tokenValido = (req, res, next) => {
     } catch (error) {
       res.status(401).json({ message: 'Token no válido.' });
     }
-  };    
+  }; 
 
-export default { Registro,login, tokenValido };
+export const obtenerUsuarios = async(req, res)=>{
+    try{
+      const usuarios = await Usuario.find();
+      res.status(200).json(usuarios)
+    } catch(error){
+      res.status(500).json({message: "Error al obtener los usuarios"})
+    }
+  };
+export const obtenerUsuario = async(req,res) =>{
+  try{
+    //Dejar que se puede leer el id de mongo
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID no válido" });
+    }
+    const usuario = await Usuario.findById(req.params.id);
+    if(!usuario) return res.status(404).json({message: "Usuario no encontrado"})
+      res.status(200).json(usuario)
+  }catch(error){
+res.status(500).json({message:"Error interno del servidor"})
+}
+}
+export const actualizarUsuario = async(req,res)=>{
+  try{
+    //Dejar que se puede leer el id de mongo
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID no válido" });
+    }
+    const usuario = await Usuario.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    if(!usuario) return res.status(404).json({message:"Usuario no encontrado"})
+    res.status(200).json(usuario);
+  }catch(error){
+    res.status(500).json({message:"Error interno del servidor"})
+  }
+}
+export const eliminarUsuario = async (req,res)=>{
+  try {
+    console.log("ID recibido", req.params.id);
+    const { id } = req.params;
+    console.log("ID perdido", req. params.id);
+    //Dejar que se puede leer el id de mongo
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "ID no válido" });
+    }
+    const usuario = await Usuario.findByIdAndDelete(req.params.id);
+    if(!usuario) return res.status(404).json({message:"Usuario no encontrado"});
+    res.status(200).json({message: "Usuario eliminado"});
+  } catch (error) {
+    res.status(500).json({message:"Error interno del servidor"});
+  }
+}
+export default { 
+  Registro,
+  login,
+   tokenValido,
+   obtenerUsuarios,
+   obtenerUsuario,
+   eliminarUsuario,
+   actualizarUsuario 
+  };
