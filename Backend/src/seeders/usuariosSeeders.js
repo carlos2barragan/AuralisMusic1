3872941980fd0362ba2f875247b1febfed9d2321
Sebas313 <rodriguezrondonsebastian@gmail.com>
2025-02-10 12:37:25 -0500

    Actualizacion de problema con los seeders

--- Backend/src/seeders/usuariosSeeders.js
+++ Backend/src/seeders/usuariosSeeders.js
@@ -1,1 +1,41 @@
+import "dotenv/config";
+import bcrypt from "bcrypt";
+import Usuarios from "../Modelos/usuariosModelos.js";
+import connectDB from "../config/database.js";
 import mongoose from "mongoose";
+import usuariosController from "../Controladores/usuariosController.js"
+
+
+(async function usuariosSeeder() {
+    try {
+      // Connect to the database
+      await connectDB();
+  
+      // Hash the password before creating the user
+      const hashedPassword = await bcrypt.hash(
+        process.env.SEEDER_USER_PASSWORD,
+        10
+      );
+  
+      // Create the user
+      const user = await Usuarios.create({
+        nombre: "Carlos Barragan",
+        email: "Carlos@starwars.com",
+        password: hashedPassword, // Use the hashed password
+      });
+  
+      // Check if the user was created successfully
+      if (user) {
+        console.log("[Seeder] User created successfully!");
+      } else {
+        console.log("[Seeder] Failed to create user.");
+      }
+  
+      // Exit the process with success
+      mongoose.connection.close();
+      process.exit(0);
+    } catch (error) {
+      // Exit with error
+      process.exit(1);
+    }
+  })();
