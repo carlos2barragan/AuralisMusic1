cbde56ed114c5d90e032fe7ca8d3e64b87eaf6be
nicolas herrera <nicoh3988@gmail.com>
2025-02-25 22:53:33 -0500

    primeros test de cantante

--- Backend/src/Controladores/usuariosController.js
+++ Backend/src/Controladores/usuariosController.js
@@ -88,7 +88,6 @@ export const verifyEmail = async (req, res) => {
     // 🔄 Si la solicitud viene del navegador (correo electrónico), redirigir al frontend
 
     res.redirect("/login?verified=true");
-=======
     res.redirect("https://auralis-music.vercel.app/login");
 
 
