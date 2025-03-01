cc1270c68dfe4254fe13aa00d1657200d675421a
carlos2barragan <bcarlosans804@gmail.com>
2025-02-28 22:56:39 -0500

    arreglo de login

--- Backend/src/rutas/usuariosrutas.js
+++ Backend/src/rutas/usuariosrutas.js
@@ -1,26 +1,17 @@
 // routes/usuarioRoutes.js
 import express from "express";
-import * as usuariosController from "../Controladores/usuariosController.js"; // Importa las funciones del controlador
+import usuariosController from "../Controladores/usuariosController.js";
+
 import sendVerificationEmailMiddleware from "../middlewares/enviarEmail.js";
 
 
 const router = express.Router();
-
 router.post("/Registro", sendVerificationEmailMiddleware, usuariosController.Registro);
-
-// Ruta para verificar el email con el token
-router.get("/verificar/:token", usuariosController.verifyEmail);
-
 router.post("/Login", usuariosController.login);
-
 router.get("/Usuario/:id", usuariosController.obtenerUsuario);
 router.get("/Usuario", usuariosController.obtenerUsuarios);
-
 router.put("/Usuario/:id", usuariosController.actualizarUsuario);
-
-
-router.patch('/usuario/:id/rol', usuariosController.updateUserRole);
-
+router.patch("/usuario/:id/rol", usuariosController.updateUserRole);
 router.delete("/Usuario/:id", usuariosController.eliminarUsuario);
 
 
