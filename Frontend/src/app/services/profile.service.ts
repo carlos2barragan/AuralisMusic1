73e3af1d929b66b20982293e3e33e3cdcb8d4e42
Carlos Barragan <bcarlosans804@gmail.com>
2026-06-18 20:17:38 -0500

    Corregir URL duplicada en editProfilePhoto y agregar headers de autenticacion

--- Frontend/src/app/services/profile.service.ts
+++ Frontend/src/app/services/profile.service.ts
@@ -1,23 +1,26 @@
-// src/app/services/profile.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
-import { environment } from '../../environments/environment'; 
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private apiUrl = `${environment.apiUrl}/Api/Usuario/`;
-  constructor(private http: HttpClient) { }
+  private apiUrl = `${environment.apiUrl}/Api/Usuario`;
 
-  // Obtener el perfil del usuario
-  getUserProfile(id: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}${id}`); 
+  constructor(private http: HttpClient) {}
+
+  private getHeaders(): HttpHeaders {
+    const token = localStorage.getItem('user_token') || '';
+    return new HttpHeaders({ Authorization: `Bearer ${token}` });
   }
+
+  getUserProfile(id: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`, { headers: this.getHeaders() });
+  }
+
   editProfilePhoto(userId: string, formData: FormData): Observable<any> {
-    return this.http.put<any>(`${this.apiUrl}/usuario/${userId}/avatar`, formData);
+    return this.http.put<any>(`${this.apiUrl}/${userId}/avatar`, formData, { headers: this.getHeaders() });
   }
 }
-
