d93ae0530800daee333c00d16bd08da000c9359a
Carlos Barragan <bcarlosans804@gmail.com>
2026-06-18 20:12:50 -0500

    Proteger todas las rutas de playlist con tokenValido

--- Backend/src/rutas/playlistrutas.js
+++ Backend/src/rutas/playlistrutas.js
@@ -1,16 +1,14 @@
 import express from "express";
 import playlistController from "../Controladores/playlistController.js";
+import tokenValido from "../middlewares/autenticacion.js";
+
 const router = express.Router();
 
-router.post("/Playlist",playlistController.crear);
+router.post("/Playlist", tokenValido, playlistController.crear);
+router.get("/Playlist", tokenValido, playlistController.listar);
+router.get("/Playlist/:id", tokenValido, playlistController.ObtenerPorId);
+router.post("/Playlist/:id", tokenValido, playlistController.agregarCancion);
+router.put("/Playlist/:id", tokenValido, playlistController.Actualizar);
+router.delete("/Playlist/:id", tokenValido, playlistController.Eliminar);
 
-router.get("/Playlist",playlistController.listar);
-router.get("/Playlist/:id",playlistController.ObtenerPorId);
-
-router.post("/Playlist/:id", playlistController.agregarCancion);
-
-
-router.put("/Playlist/:id",playlistController.Actualizar);
-
-router.delete("/Playlist/:id",playlistController.Eliminar);
-export default router
+export default router;
