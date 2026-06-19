eff284a8594ae3d4065e962a9d18a18b461d98b6
Carlos Barragan <bcarlosans804@gmail.com>
2026-06-18 20:13:02 -0500

    Agregar tokenValido y verificarRoles a rutas de cantantes

--- Backend/src/rutas/cantantesrutas.js
+++ Backend/src/rutas/cantantesrutas.js
@@ -1,15 +1,14 @@
 import express from "express";
 import cantanteController from "../Controladores/cantanteController.js";
+import tokenValido from "../middlewares/autenticacion.js";
 import verificarRoles from "../middlewares/verificarRole.js";
-const router =express.Router();
 
-router.post("/Cantante",cantanteController.crearCantante)
-router.get("/Cantante",cantanteController.listarCantantes);
-router.get("/Cantante/:id",cantanteController.obtenerCantante);
+const router = express.Router();
 
-router.put("/Cantante/:id",cantanteController.actualizarCantante);
+router.post("/Cantante", tokenValido, verificarRoles(["administrador"]), cantanteController.crearCantante);
+router.get("/Cantante", cantanteController.listarCantantes);
+router.get("/Cantante/:id", cantanteController.obtenerCantante);
+router.put("/Cantante/:id", tokenValido, verificarRoles(["administrador", "cantante"]), cantanteController.actualizarCantante);
+router.delete("/Cantante/:id", tokenValido, verificarRoles(["administrador"]), cantanteController.eliminarCantante);
 
-router.delete("/Cantante/:id",verificarRoles(["administrador"]),cantanteController.eliminarCantante);
-
-
-export default router
+export default router;
